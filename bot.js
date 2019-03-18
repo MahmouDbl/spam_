a8a1890bb9abeb0b7d5b5b8a5cb1bd7fe67e51aa
MahmouDbl <48621739+MahmouDbl@users.noreply.github.com>
2019-03-18 18:19:38 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,14 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get(
+"355023283055230976")
+setInterval(function() {
+channel.send(`my channel is Mahmoud Bl`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
